0534188b86d4fed15d2e0eb15d50193191f98139
Daniel Batista <dani_batista20@hotmail.com>
2025-04-30 13:03:37 +0100

    feat: enhance App component with button click handler and layout adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Button } from '@/components/ui/button'
+
 function App() {
+  const HandleButtonClick = () => {
+    console.log('Button clicked')
+  }
+
   return (
-    <section className='flex flex-col items-center justify-center h-screen'>
+    <section className="flex flex-col items-center justify-center h-screen gap-10">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button onClick={HandleButtonClick} className="cursor-pointer">
+        Click me
+      </Button>
     </section>
   )
 }
