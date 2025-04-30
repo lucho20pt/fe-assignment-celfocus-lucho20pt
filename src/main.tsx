ba2a345b434fb3320d988772e0c82c516856231b
Daniel Batista <dani_batista20@hotmail.com>
2025-04-30 23:29:28 +0100

    feat: integrate QueryClient for data fetching in main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from '@/App'
+import '@/index.css'
 
-createRoot(document.getElementById('root')!).render(
+// Create a QueryClient instance
+const queryClient = new QueryClient()
+console.log(queryClient)
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
 )
